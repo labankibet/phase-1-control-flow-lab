c59266bf2effb769baf8813889decf0e8c359578
labankibet <laban.kibet@moringaschool.com>
2023-07-16 20:18:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue)
   // Write your code here!
+  function addFive(someNumber) {
+    let result
+  if (someNumber > 0) {
+    result = someNumber + 5;
+    return result
+  }
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  addFive(addFive(5));
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  Number(true);
 }
